f219c287844a25830c7cef05c28efdb825b8138a
Yağmur Sayın <yagmur232367@gmail.com>
2025-10-28 21:23:03 +0300

    exc5 tamam

--- ex5.c
+++ ex5.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+ void sayHello(char *name)
+ {
+    printf("Hello, %s!\n",name);
+ }
+ int main(){
+    printf("Please enter your name : ");
+    char userName[40];
+    scanf("%s",userName);
+    sayHello(userName);
+    return 0;
+ }
