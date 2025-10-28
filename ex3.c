5fb4dec4d59c902bc239bb48a4a90bd5469f337a
Yağmur Sayın <yagmur232367@gmail.com>
2025-10-28 20:58:15 +0300

    ex3 tamam

--- ex3.c
+++ ex3.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char pet [10];
+    printf("Do you have cat or dog? : ");
+    scanf("%s",pet);
+    if (strcmp(pet,"cat") == 0){
+        printf("MİYAVVVV\n");
+    } else if (strcmp(pet,"dog") ==0){
+        printf("HAV HAVVVV\n");
+    }else
+    {
+        printf("I don't know that animal sound.\n");
+    }
+    
+    return 0;
+    }
+
 
 
