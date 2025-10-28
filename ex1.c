cf4afcf9ccf194c4abb5206da01c968a3862ad3c
Yağmur Sayın <yagmur232367@gmail.com>
2025-10-28 18:25:16 +0300

    excersize 1 tamam

--- ex1.c
+++ ex1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
 
+ int magic_number = 3;
+ int guess;
+ printf("Please enter a number between 1 and 5 : ");
+   scanf("%d", &guess);
+   if (guess == magic_number) {
+    printf("You guessed it!\n");
+   } else {
+    printf("Sorry, u couldn't guess it\n");
+   }
+
+
+  return 0;
+}
