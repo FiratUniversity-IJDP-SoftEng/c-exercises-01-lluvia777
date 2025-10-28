bcdc8c30d6910b8e9a50481b989304a085dd1253
Yağmur Sayın <yagmur232367@gmail.com>
2025-10-28 20:41:28 +0300

    excersize 2 tamam

--- ex2.c
+++ ex2.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+int main() {
+    printf("Please enter your age : ");
+    int age;
+    scanf("%d", &age);
+    for( int i = 0; i < age; i++) {
+        printf("Happy Birthday!\n");
+    }
+    return 0;
+}
